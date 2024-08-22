88ccd3d63a6268165d214295160414a6a7977d28
Poojan Patel <p.poojan1762002@gmail.com>
2024-08-22 12:04:17 +0530

    thru Normal Css

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 gsap.to("#box1",{
     x:1000,
     duration:2,
-    delay:1
+    delay:1,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    scale:0.5,
 })
 gsap.from("#box2",{
     x:1200,
