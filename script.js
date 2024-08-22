fe0f990a615bc7e929c31aa1531ab780e20d4193
Poojan Patel <p.poojan1762002@gmail.com>
2024-08-22 12:43:58 +0530

    gsap.timeline

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-gsap.to("#box1", {
-    x: 1000,
-    duration: 2,
-    delay: 1,
-    rotate: 360,
-    backgroundColor: "blue",
-    borderRadius: "50%",
-    scale: 0.5,
-    // repeat:1,  here once time going to forward and backward
-    repeat: -1, //here perform infinite time 
-    yoyo: true, 
+const tl=gsap.timeline()
+tl.to("#box1",{
+    x:"1200px",
+    duration:1,
+    delay:1
 })
-gsap.from("#box2", {
-    x: 1200,
-    duration: 2,
-    delay: 1.5
+tl.to("#box2",{
+    x:"1200px",
+    duration:1, 
 })
-gsap.from("h1", {
-    opacity: 0,
-    y: 20,
-    duration: 1,
-    delay: 0.5,
-    stagger: 1
+tl.to("#box3",{
+    x:"1200px",
+    duration:1, 
 })
